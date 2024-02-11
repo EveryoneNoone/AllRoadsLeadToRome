ea15515b92604ad91b7171b66fcede183a38eddb
Aleksandr Smirnov <aleksandr.smirnov2009@gmail.com>
2024-02-11 19:54:26 +0200

    Create order, address and status entities

---
+++ Domain/Domain.Entities/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Address
+    {
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+    }
+}
