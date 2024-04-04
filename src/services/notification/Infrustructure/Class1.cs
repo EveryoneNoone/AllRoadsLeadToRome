4f28324a2ad4591f25aef870a7e5c71e0f0e5dba
Aleksandr Smirnov <aleksandr.smirnov2009@gmail.com>
2024-04-04 22:22:20 +0200

    Infrastructure of notification service

---
+++ src/services/notification/Infrustructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrustructure
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src/services/notification/NotificationServiceTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace NotificationServiceTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
